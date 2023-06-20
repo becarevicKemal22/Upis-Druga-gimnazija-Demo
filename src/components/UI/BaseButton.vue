a0120efe9075c2a3f65a9bfeb232929cb1846935
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2023-06-20 17:42:34 +0200

    Minor button change

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <button class="p-2 px-3 rounded-md bg-blue-800 text-white font-open-sans hover:scale-105 transition-transform w-fit">
+  <button class="p-2 px-4 rounded-md bg-blue-800 text-white font-open-sans hover:scale-105 transition-transform w-fit">
     <slot></slot>
   </button>
 </template>

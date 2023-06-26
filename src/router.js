ede74805bef606fb974580f3271bb32ed1e39bae
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2023-06-26 18:54:07 +0200

    Router fix

--- src/router.js
+++ src/router.js
@@ -1,13 +1,23 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import MainView from "@/views/MainView.vue";
 import AnketaView from '@/views/AnketaView.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
             name: 'main',
+            component: MainView,
+        },
+        {
+            path: '/Upis-Druga-gimnazija-Sarajevo/',
+            name: 'mainDeploy',
+            component: MainView,
+        },
+        {
+            path: '/Upis-Druga-gimnazija-Sarajevo/generacijaAnkete/',
+            name: 'generacijaAnketeDeploy',
             component: MainView,
         },
         {
